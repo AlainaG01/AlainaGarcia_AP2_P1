7c476e48ff8ecd1abf2dcb098e70994e4854231c
alainagarcia <alaina_garcia@ucne.edu.do>
2025-05-28 20:44:18 -0400

    Configura inyeccion dependencias con Hilt

---
+++ app/src/main/java/edu/ucne/alainagarcia_ap2_p1/AlainaGarcia_AP2_P1App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.alainagarcia_ap2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AlainaGarcia_AP2_P1App: Application() {
+
+}
