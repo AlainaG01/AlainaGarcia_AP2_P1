45e96b1773279593c2518df4c374d2e6895644b4
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-04 18:52:23 -0400

    Base de datos realizado

---
+++ app/src/main/java/edu/ucne/alainagarcia_ap2_p1/data/local/database/TareaDb.kt
@@ -0,0 +1,17 @@
+package edu.ucne.alainagarcia_ap2_p1.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.alainagarcia_ap2_p1.data.local.dao.TareaDao
+import edu.ucne.alainagarcia_ap2_p1.data.local.entities.TareaEntity
+
+@Database(
+    entities = [
+        TareaEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class TareaDb: RoomDatabase(){
+    abstract fun TareaDao(): TareaDao
+}
