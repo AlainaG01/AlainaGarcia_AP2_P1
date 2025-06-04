82e97d23c7bfe095a60485ff3f5190ea5d033938
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-04 18:52:40 -0400

    Repository realizado

---
+++ app/src/main/java/edu/ucne/alainagarcia_ap2_p1/data/repository/TareaRepository.kt
@@ -0,0 +1,15 @@
+package edu.ucne.alainagarcia_ap2_p1.data.repository
+
+import edu.ucne.alainagarcia_ap2_p1.data.local.dao.TareaDao
+import edu.ucne.alainagarcia_ap2_p1.data.local.entities.TareaEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class TareaRepository @Inject constructor(
+    private val dao: TareaDao
+){
+    suspend fun save(tarea: TareaEntity) = dao.save(tarea)
+    suspend fun find(tareaId: Int) = dao.find(tareaId)
+    suspend fun delete(tarea: TareaEntity) = dao.delete(tarea)
+    fun getAll(): Flow<List<TareaEntity>> = dao.getAll()
+}
